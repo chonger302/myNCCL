e572e5f767c95b5cf39067e85acb8ed8c0391b14
chonger302 <chonger302@163.com>
2012-11-25 13:49:16 +0800

    Update homework-1.c

--- homework-1.c
+++ homework-1.c
@@ -19,4 +19,12 @@ int main(void
 }
 prog.c:2: error: expected ‘;’, ‘,’ or ‘)’ before ‘{’ token
 
+#4
+int main(void)
+{
+    return 0
+}
+prog.c: In function ‘main’:
+prog.c:4: error: expected ‘;’ before ‘}’ token
+
 
