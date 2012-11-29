56de147fc7a41b48750747889413014e05fa603f
chonger302 <chonger302@163.com>
2012-11-29 21:30:20 +0800

    Update homework-2.c

--- homework-2.c
+++ homework-2.c
@@ -48,101 +48,34 @@ c at 0xbfdd2bfc
 /* 第一题回答：全局变量跟局部变量在系统编译时在内存中分别放在不同的存储单元。*/
 
 3
-#include <stdio.h>
-
-int global = 100;
-
-int main(void)
+#include "stdio.h"
+int global;
+void main()
 {
+  printf("global=%p &global+1=%p\n",(int)&global,(int)(&global+1));
+}
 
-    printf("global+1 at %p\n", &global+1);
-    
-    return 0;
-}   
+global=004237A0 &global+1=004237A4
 
- input: 無
-output:
-
-global+1 at 0x804a018
-
-//第三题答案 
+//第二题答案 int 类型全局变量地址加1的值相当于在原来地址的基础上加了4
 
 4
-#include <stdio.h>
-
-int   a = 100;
-char  b = 101;
-short c = 102;
-float d = 103;
-
-int main(void)
+#include "stdio.h"
+int    global_1;
+char   global_2;
+short  global_3;
+float  global_4;
+void main()
 {
-    
-    printf("a at %p\n", &a);
-    printf("b at %p\n", &b);
-    printf("c at %p\n", &c);
-    printf("d at %p\n", &d);
+  printf("global_1=%p &global_1+1=%p\n",(int)&global_1,(int)(&global_1+1));
+  printf("global_2=%p &global_2+1=%p\n",(int)&global_2,(int)(&global_2+1));
+  printf("global_3=%p &global_3+1=%p\n",(int)&global_3,(int)(&global_3+1));
+  printf("global_4=%p &global_4+1=%p\n",(int)&global_4,(int)(&global_4+1));
+}
 
-    return 0;
-}   
-input: 無
-output:
+global_1=004237B0 &global_1+1=004237B4
+global_2=004237B4 &global_2+1=004237B5
+global_3=004237A0 &global_3+1=004237A2
+global_4=004237AC &global_4+1=004237B0
 
-a at 0x804a014
-b at 0x804a018
-c at 0x804a01a
-d at 0x804a01c
-
-5
-#include <stdio.h>
-
-int   a = 100;
-char  b = 101;
-short c = 102;
-float d = 103;
-
-int main(void)
-{
-    
-    printf("a at %p\n", a);
-    printf("b at %p\n", b);
-    printf("c at %p\n", c);
-    printf("d at %p\n", d);
-
-    return 0;
-}   
-input: 無
-output:
-
-a at 0x64
-b at 0x65
-c at 0x66
-d at (nil)
-
-// 第三题回答 不同的数据类型在内存中的存储地址也不一样
-
-6
-#include <stdio.h>
-
-int   a = 100;
-char  b = 101;
-short c = 102;
-float d = 103;
-
-int main(void)
-{
-    
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-    printf("c = %d\n", c);
-    printf("d = %d\n", d);
-
-    return 0;
-}   
- input: 無
-output:
-
-a = 100
-b = 101
-c = 102
-d = 0
+//第二题答案  各数据类型不同，加1 再原来地址上加的字节数也不同
