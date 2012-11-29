09a4cd9566f6e7a4c0ccd02163f4025440f2d037
chonger302 <chonger302@163.com>
2012-11-29 23:17:59 +0800

    Update homework-4.c

--- homework-4.c
+++ homework-4.c
@@ -1,1 +1,24 @@
-#ing
+#include <stdio.h>
+
+int main(void)
+{
+    int A,B;
+    
+    printf("please input a number A : ");
+    scanf("%d", &A);
+    
+    while ( A != B )
+       
+     {
+       printf("please input a number B : ");
+        scanf("%d", &B);
+        
+        if   (A < B )
+         printf("max\n");
+        if   (A > B )   
+         printf("mix!\n");   
+     } 
+     
+    printf("very,good!\n");
+    return 0;
+}
