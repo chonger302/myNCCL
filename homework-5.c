07db886214032059a67631359a66a735a1ab00ae
chonger302 <chonger302@163.com>
2012-12-03 13:41:18 +0800

    Update homework-5.c

--- homework-5.c
+++ homework-5.c
@@ -1,1 +1,26 @@
-eee
+*******************************************************************************
+### 1
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int sum = 0;
+    int value;
+    int average;
+    
+    for ( i = 0; i < 5; i++)
+      {
+        printf("please input an integer number: \n");
+        scanf("%d",&value);
+        sum+=value;
+      }
+
+    average=sum/5;
+    printf("The average is : %d\n",average);
+  
+    return 0;
+}
+
+
+
