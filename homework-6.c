6e6b342611bcc8d7478c0ec226352fcf89f52b49
chonger302 <chonger302@163.com>
2012-12-04 19:59:43 +0800

    Update homework-6.c

--- homework-6.c
+++ homework-6.c
@@ -45,3 +45,24 @@ int main(void)
     return 0;
 }
 
+**********************************************************************************
+### 3
+#include <stdio.h>
+
+ int main(void)
+   {
+      int i, j, k, n;
+       
+      for(n=100;n<1000;n++)
+         {
+           i=n/100;
+           j=n/10%10;
+           k=n%10;
+  
+           if(i*100+j*10+k==i*i*i+j*j*j+k*k*k)
+             printf("water flower number is %d!\n",n);
+         }
+          
+       return 0;
+    }
+    
