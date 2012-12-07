0f30b0d8304936750775615e1f635ce42e13ec8b
chonger302 <chonger302@163.com>
2012-12-07 13:03:04 +0800

    Update homework-8.c

--- homework-8.c
+++ homework-8.c
@@ -1,1 +1,39 @@
-ww
+****************************************************************************
+###  1
+
+#include <stdio.h>
+#include <math.h>
+
+ int  prime(int x)
+
+   {
+      int j;
+
+      for ( j = 2; j <= sqrt(x); j++)
+        {
+          if ( x % j == 0)
+            return 0;
+        }
+
+      return 1;
+    
+    }
+
+  
+  int main ( void )
+
+    {
+      int i;
+      int max = 0;
+
+      for ( i = 2; i <= 100; i++)
+         {
+            if (prime ( i ) == 1)
+              max = i;
+         }
+
+       printf("%d\n",max);
+
+       return 0;
+
+     }
